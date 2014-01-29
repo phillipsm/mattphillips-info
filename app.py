f43d66156d69e92fd4a830cd2d48306e3e102075
Matthew Phillips <matt@Rhondas-iMac.local>
2014-01-28 21:21:05 -0500

    a little more flask-ifying

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import re 
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -8,7 +6,11 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def landing():
-    return render_template('landing.html', num_millisecs=0)
+    return render_template('landing.html')
+    
+@app.route('/projects/ambient-timer')
+def projects_ambient_timer():
+    return render_template('projects/ambient-timer.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
