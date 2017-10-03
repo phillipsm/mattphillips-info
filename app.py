03ff114698951dd20200ebffffc70cf037681d47
Matthew Phillips <matthewphillips@Matthews-MacBook-Pro.local>
2017-10-03 12:52:26 -0600

    quick site update. things are pretty crufty. this site still needs plenty of love

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def page_not_found(e):
 @app.route('/ambient-timer')
 def projects_ambient_timer():
     return render_template('projects/ambient-timer.html', project_title="Ambient Timer")
-    
+
 @app.route('/awesome-box')
 def projects_awesome_box():
     return render_template('projects/awesome-box.html', project_title="Awesome Box")
@@ -27,10 +27,6 @@ def projects_awesome_box():
 @app.route('/hovermarks')
 def projects_hovermarks():
     return render_template('projects/hovermarks.html', project_title="Hovermarks")
-
-@app.route('/working-in-widener')
-def projects_working_in_widener():
-    return render_template('projects/working-in-widener.html', project_title="Working in Widener")
 
 @app.route('/pyfav')
 def projects_pyfav():
@@ -47,7 +43,7 @@ def projects_360():
 @app.route('/360/hand')
 def projects_360_hand():
     return render_template('projects/360-hand.html')
-    
+
 @app.route('/frames')
 def projects_frames():
     return render_template('projects/frames.html', project_title="Frames")
@@ -58,5 +54,3 @@ def projects_stereo():
 
 if __name__ == '__main__':
     app.run(debug=False)
-    
-    
